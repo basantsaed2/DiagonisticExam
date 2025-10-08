913e588f7744ba62197344dbdadd3bf1ab7a0f25
basantsaed2 <basantsaed718@gmail.com>
2025-10-08 11:42:54 +0300

    add dia_exam basename in vitconfig , and router

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,45 +11,51 @@ import BuyChapter from "./Pages/DiaExam/BuyChapter";
 import ForgetPassword from "./Pages/Authentication/ForgetPassword";
 
 
-export const router = createBrowserRouter([
-  {
-    path: "",
-    element: <App />,
-    children: [
-      // Guest-only (auth pages)
-      {
-        path: "",
-        element: <ProtectedLogin />, // checks if user exists → redirect to "/"
-        children: [
-          { path: "/", element: <Login /> },
-          { path: "signup", element: <SignUp /> },
-          { path: "forget_password", element: <ForgetPassword /> },
-        ],
-      },
+export const router = createBrowserRouter(
+  [
+    {
+      path: "",
+      element: <App />,
+      children: [
+        // Guest-only (auth pages)
+        {
+          path: "",
+          element: <ProtectedLogin />, // checks if user exists → redirect to "/"
+          children: [
+            { path: "/", element: <Login /> },
+            { path: "signup", element: <SignUp /> },
+            { path: "forget_password", element: <ForgetPassword /> },
+          ],
+        },
 
-      // Protected routes (require user login)
-      {
-        path: "",
-        element: <ProtectedLogin />, // checks if !user → redirect to "/login"
-        children: [
-          {
-            path: "courses",
-            element: <Courses />,
-          },
-          {
-            path: "exam/:courseId",
-            element: <Exam />,
-          },
-          {
-            path: "exam/results/:examId",
-            element: <ExamResult />,
-          },
-          {
-            path: 'buy_chapters',
-            element: <BuyChapter />
-          },
-        ],
-      },
-    ],
-  },
-]);
+        // Protected routes (require user login)
+        {
+          path: "",
+          element: <ProtectedLogin />, // checks if !user → redirect to "/login"
+          children: [
+            {
+              path: "courses",
+              element: <Courses />,
+            },
+            {
+              path: "exam/:courseId",
+              element: <Exam />,
+            },
+            {
+              path: "exam/results/:examId",
+              element: <ExamResult />,
+            },
+            {
+              path: 'buy_chapters',
+              element: <BuyChapter />
+            },
+          ],
+        },
+      ],
+    },
+  ]
+  ,
+  {
+    basename: "/diagnoistic_exam",
+  }
+);
